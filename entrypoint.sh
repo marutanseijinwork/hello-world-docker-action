d372324c11ece498d9cdd0381e22802e1241803b
marutanseijinwork <ryo.maruta@progrit.co.jp>
2022-07-06 23:39:07 +0900

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ gem install github_changelog_generator
 gem which github_changelog_generator
 github_changelog_generator -u marutanseijinwork -p hello-world-docker-action
 echo $PATH
+rb -v
 echo "こんばんは $1"
 time=$(date)
 echo "::set-output name=time::$time"
