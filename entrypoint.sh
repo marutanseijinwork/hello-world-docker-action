13cb1673d8375091cd7aef9199438e64e4f0e37c
marutanseijinwork <ryo.maruta@progrit.co.jp>
2022-07-07 00:34:13 +0900

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ gem environment
 pwd
 ls -a .
 cat ~/.bashrc
+cd /usr/local/bundle/gems
 github_changelog_generator -u marutanseijinwork -p hello-world-docker-action
 echo $PATH
 ruby github_changelog_generator.rb -u marutanseijinwork -p hello-world-docker-action
