4f9c679ad9cad348aa2b4b467e72617429f44d44
marutanseijinwork <ryo.maruta@progrit.co.jp>
2022-07-06 23:52:31 +0900

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -l
 
-gem install github_changelog_generator
+gem install github_changelog_generator 
 gem which github_changelog_generator
+gem environment
+cat ~/.bash_profile
 github_changelog_generator -u marutanseijinwork -p hello-world-docker-action
 echo $PATH
 ruby github_changelog_generator.rb -u marutanseijinwork -p hello-world-docker-action
